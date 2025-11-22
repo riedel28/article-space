6f8f1bdc612c0a82e79db2554f906a618b4fee23
Sergey Riedel <sergey.riedel@gmail.com>
2025-11-22 10:44:39 +0100

    chore(config): enhance Vite configuration for improved development experience
    - Added path and URL utilities to resolve the source directory correctly.
    - Updated the alias configuration to use an absolute path for better reliability.
    - Configured the development server to open automatically on port 5173.
    
    This commit improves the Vite setup, ensuring a smoother development workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [svgr({ exportAsDefault: true }), react()],
+    plugins: [react(), svgr()],
     resolve: {
-        alias: [{ find: '@', replacement: '/src' }],
+        alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }]
+    },
+    server: {
+        port: 5173,
+        open: true
     },
     define: {
         __IS_DEV__: JSON.stringify(true),
         __API__: JSON.stringify('http://localhost:8000'),
-        __PROJECT__: JSON.stringify('frontend'),
-    },
+        __PROJECT__: JSON.stringify('frontend')
+    }
 });
