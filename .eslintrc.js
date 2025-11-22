2f8bc405a0d94f49f4df10a6b918111b8bf0bfcf
Sergey Riedel <sergey.riedel@gmail.com>
2025-11-22 11:59:50 +0100

    refactor(ui): update Avatar, Button, and DropdownMenu components for improved structure and functionality
    - Refactored `Avatar`, `Button`, and `DropdownMenu` components to enhance readability and maintainability.
    - Introduced new functional component structures and improved class name management.
    - Removed deprecated ESLint rule for maximum line length to streamline code style.
    - Added `lucide-react` as a dependency for icon support in dropdown menus.
    
    These changes enhance the overall code quality and user interface consistency across the application.

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,13 +69,6 @@ module.exports = {
                 ]
             }
         ],
-        'max-len': [
-            'error',
-            {
-                ignoreComments: true,
-                code: 125
-            }
-        ],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error',
