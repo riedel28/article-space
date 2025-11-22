dd9ef6abac30e1d8bc3370cea60650fdc1f8a062
Sergey Riedel <sergey.riedel@gmail.com>
2025-11-22 10:56:50 +0100

    feat(types): add support for SVG modules with React
    - Introduced a new type declaration for SVG files with the `?react` query, allowing for better integration of SVGs as React components.
    - This change enhances type safety and improves the developer experience when working with SVG assets in React applications.

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -21,6 +21,13 @@ declare module '*.svg' {
     export default SVG;
 }
 
+declare module '*.svg?react' {
+    import React from 'react';
+
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
+
 declare const __IS_DEV__: boolean;
 declare const __API__: string;
 declare const __PROJECT__: 'storybook' | 'frontend' | 'jest';
