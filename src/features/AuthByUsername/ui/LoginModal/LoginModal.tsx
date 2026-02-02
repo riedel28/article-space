1e085912b48dc96388449447f473c65f657a669e
Sergey Riedel <sergey.riedel@gmail.com>
2026-02-02 16:01:30 +0100

    feat: implement responsive design for LoginModal
    - Added Drawer component for mobile view to enhance user experience.
    - Refactored title and content rendering for better code organization and maintainability.
    - Utilized useMediaQuery to adapt the modal display based on screen size.

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,4 +1,5 @@
-import { Modal, Skeleton, Stack, Text, Title } from '@mantine/core';
+import { Drawer, em, Modal, Skeleton, Stack, Text, Title } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -26,27 +27,49 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   const { t } = useTranslation();
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+  const title = (
+    <Stack gap={4}>
+      <Title fw={600} order={2} fz="lg">
+        {t('Добро пожаловать')}
+      </Title>
+      <Text size="sm" c="dimmed">
+        {t('Введите свои данные для входа')}
+      </Text>
+    </Stack>
+  );
+
+  const content = (
+    <Suspense fallback={<LoginFormSkeleton />}>
+      <LoginFormAsync onSuccess={onClose} />
+    </Suspense>
+  );
+
+  if (isMobile) {
+    return (
+      <Drawer
+        className={className}
+        opened={isOpen}
+        onClose={onClose}
+        title={title}
+        position="bottom"
+        size="auto"
+      >
+        {content}
+      </Drawer>
+    );
+  }
 
   return (
     <Modal
       className={className}
       opened={isOpen}
       onClose={onClose}
-      title={
-        <Stack gap={4}>
-          <Title fw={600} order={2} fz="lg">
-            {t('Добро пожаловать')}
-          </Title>
-          <Text size="sm" c="dimmed">
-            {t('Введите свои данные для входа')}
-          </Text>
-        </Stack>
-      }
+      title={title}
       centered
     >
-      <Suspense fallback={<LoginFormSkeleton />}>
-        <LoginFormAsync onSuccess={onClose} />
-      </Suspense>
+      {content}
     </Modal>
   );
 };
